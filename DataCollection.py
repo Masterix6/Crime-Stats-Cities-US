018de1677d8372dbe3abb14cae84dd0e9b4483ac
Masterix6 <162488290+Masterix6@users.noreply.github.com>
2024-07-09 20:39:09 +0200

    Merge branch 'main' of https://github.com/Masterix6/Crime-Stats-Cities-US

--- DataCollection.py
+++ DataCollection.py
@@ -1,23 +1,27 @@
-# script for getting the data from the api and removing unneccessary keys
+# script for getting the data from the api, removing unneccessary keys and converts it into a (for the programe) readable table
 
 import pandas
 
 keys_for_analyzing = { # keys which will be kept
-    "start_date": {"NY": "cmplnt_fr_dt",},
-    "start_time": {"NY": "cmplnt_fr_tm",},
-    "end_date": {"NY": "cmplnt_to_dt",},
-    "end_time": {"NY": "cmplnt_to_tm",},
-    "report_date": {"NY": "rpt_dt",},
+    "NY": {
+        "cmplnt_fr_dt": "start_date",
+        "cmplnt_fr_tm": "start_time",
+        "cmplnt_to_dt": "end_date",
+        "cmplnt_to_tm": "end_time",
+        "rpt_dt": "report_date",
+        "": "",
 
+
+    }
 } 
 
 data_bases = { # Structure: "City(short)": api # website
-    "NY": "https://data.cityofnewyork.us/resource/qgea-i56i.json?$limit=50000" # https://data.cityofnewyork.us/Public-Safety/NYPD-Complaint-Data-Historic/qgea-i56i/about_data
-
+    "NY": "https://data.cityofnewyork.us/resource/qgea-i56i.json?$limit=50000", # https://data.cityofnewyork.us/Public-Safety/NYPD-Complaint-Data-Historic/qgea-i56i/about_data
+    "LA": "https://data.lacity.org/resource/63jg-8b9z.json?$limit=50000",
 }
 
 
 def getDataFrame(city: str):
 
 
-    return ""
+    return {}
