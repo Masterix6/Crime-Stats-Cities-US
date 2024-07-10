7846c5a8cc5ee6baa424a35d6df9dd90354ae41d
Masterix6 <162488290+Masterix6@users.noreply.github.com>
2024-07-10 19:09:31 +0200

    Merge branch 'main' of https://github.com/Masterix6/Crime-Stats-Cities-US

--- DataCollection.py
+++ DataCollection.py
@@ -1,18 +1,7 @@
-# script for getting the data from the api, removing unneccessary keys and converts it into a (for the programe) readable table
+# script for getting the data from the api, removing unneccessary keys and converts it into a (for the program) readable dict
 
 import pandas
-TypesOfInformationCities={"NY":{"Reported":["start_date","start_time",'borough']},"La":""}
-while True:
-    data_bases=input("What city do you want to look at, (about) to see what citys you can choose from, q to quit: ")
-    data_bases.lower()
-    if data_bases == "about":
-        print(TypesOfInformationCities)
-        continue
-    elif data_bases=="q":
-        quit()
-    wanteddatatype=input("docuement do you want:")
-    Wanteddata=input("what statistic do you want: ")
-    break
+
 keys_for_analyzing = { # keys which will be kept
     "NY": {
         "cmplnt_fr_dt": "start_date",
@@ -20,11 +9,14 @@ keys_for_analyzing = { # keys which will be kept
         "cmplnt_to_dt": "end_date",
         "cmplnt_to_tm": "end_time",
         "rpt_dt": "report_date",
+        
         "law_cat_cd": "lvl_offense",
         "boro_nm": "borough",
+
         "susp_age_group": "suspect_age",
         "susp_race": "suspect_race",
         "susp_sex": "suspect_sex",
+
         "latitude": "latitude",
         "longitude": "longitude",
         "lat_lon": "location",
@@ -37,26 +29,30 @@ data_bases = { # Structure: "City(short)": api # website
     "LA": "https://data.lacity.org/resource/63jg-8b9z.json?$limit=50000",
 }
 
-
+# get the Dataframe of choosen city with only valid coloums (to analyze)
 def getDataFrame(city: str):
-    data_base = None
-    label_conversion = None
+    data_base = None # data base from city crime api - none because value will be tried to set with 'try'
+    label_conversion = None # conversion table (keys_for_analyzing) - also being set with 'try'
+
+    # try to find city crime api
     try:
         data_base = data_bases[city]
     except:
         print("City not found")
-        return False
+        return False # error with finding city's crime api in api/data_bases dict (did not find)
     
+    # try to find conversion table
     try:
         label_conversion = keys_for_analyzing[city]
     except:
         print("City not found")
-        return False    
+        return False # error with finding city in conversion table (did not find)
     
-    data_base = pandas.read_json(data_base)
-    to_drop = list(data_base.columns) - label_conversion.keys()
+    
+    data_base = pandas.read_json(data_base) # get json data from api with pandas
+    to_drop = list(data_base.columns) - label_conversion.keys() # set list of keys which are not eligible for analyzing
 
-    data_base.drop(to_drop, axis=1, inplace=True)
-    data_base = data_base.rename(columns=label_conversion)
+    data_base.drop(to_drop, axis=1, inplace=True) # removing not eligible keys
+    data_base = data_base.rename(columns=label_conversion) # rename keys to (for program) readable format
     
     return data_base
