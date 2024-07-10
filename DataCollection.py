9fe104743705db1077e0eae35ff46ae1bbc52063
theannoyingkid12 <ianbenmorrison@gmail.com>
2024-07-10 10:34:01 -0400

    made some updates to the criteria
    still working on it but it is getting better

--- DataCollection.py
+++ DataCollection.py
@@ -1,7 +1,10 @@
 # script for getting the data from the api, removing unneccessary keys and converts it into a (for the programe) readable table
 
 import pandas
-TypesOfInformationCities={"NY":{"Reported":["start_date","start_time",'borough']},"La":""}
+#this is where we will store the opions for what you can choose
+TypesOfInformationCities={"NY":{"Reported":["start_date","start_time","borough","suspect","victum"]},
+                          "La":{"2010-2019":["area","victum","datetime_occured","location","crime"]},
+                          "La":{"2020-current":["area","victum","datetime_occured","location","crime"]}}
 while True:
     data_bases=input("What city do you want to look at, (about) to see what citys you can choose from, q to quit: ")
     data_bases.lower()
@@ -23,14 +26,10 @@ keys_for_analyzing = { # keys which will be kept
         ["vic_age_group","vic_race","vict_sex"]:"victum",
     "LA":{
         "crm_cd_desc":"crime",
-        "area":"area",
-        "rpt_dist_no":"location_code",
-        "vict_sex":"victums_sex",
-        "vict_age":"victums_age",
-        "vict_descent":"victum descent",
-        "premis_cd":"premis_area",
-        "date_occ":"date_occured",
-        "time_occ":"time_occured",
+        ["area","rpt_dist_no","premis_cd"]:"area",
+        ["vict_sex","vict_age","vict_descent"]:"victum",
+        ["date_occ","time_occ"]:"datetime_occured",
+        ["latitude","longitude"]:"location",
     }
 
 
