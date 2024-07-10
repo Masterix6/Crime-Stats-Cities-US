c625c74f22e668cd9cfd03b6906a3ebec9ea7a0d
Masterix6 <162488290+Masterix6@users.noreply.github.com>
2024-07-10 16:21:14 +0200

    DataCollection getDataFrame function + NY added

--- DataCollection.py
+++ DataCollection.py
@@ -9,19 +9,43 @@ keys_for_analyzing = { # keys which will be kept
         "cmplnt_to_dt": "end_date",
         "cmplnt_to_tm": "end_time",
         "rpt_dt": "report_date",
-        "": "",
-
+        "law_cat_cd": "lvl_offense",
+        "boro_nm": "borough",
+        "susp_age_group": "suspect_age",
+        "susp_race": "suspect_race",
+        "susp_sex": "suspect_sex",
+        "latitude": "latitude",
+        "longitude": "longitude",
+        "lat_lon": "location",
 
     }
 } 
 
 data_bases = { # Structure: "City(short)": api # website
-    "NY": "https://data.cityofnewyork.us/resource/qgea-i56i.json?$limit=50000", # https://data.cityofnewyork.us/Public-Safety/NYPD-Complaint-Data-Historic/qgea-i56i/about_data
+    "NY": "https://data.cityofnewyork.us/resource/qgea-i56i.json?$limit=1000", # https://data.cityofnewyork.us/Public-Safety/NYPD-Complaint-Data-Historic/qgea-i56i/about_data
     "LA": "https://data.lacity.org/resource/63jg-8b9z.json?$limit=50000",
 }
 
 
 def getDataFrame(city: str):
+    data_base = None
+    label_conversion = None
+    try:
+        data_base = data_bases[city]
+    except:
+        print("City not found")
+        return False
+    
+    try:
+        label_conversion = keys_for_analyzing[city]
+    except:
+        print("City not found")
+        return False    
+    
+    data_base = pandas.read_json(data_base)
+    to_drop = list(data_base.columns) - label_conversion.keys()
 
-
-    return {}
+    data_base.drop(to_drop, axis=1, inplace=True)
+    data_base = data_base.rename(columns=label_conversion)
+    
+    return data_base
