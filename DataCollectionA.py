5f21f79eb4b7af3b5dc1353ed2c736eed8c4f556
Masterix6 <162488290+Masterix6@users.noreply.github.com>
2024-07-11 16:27:17 +0200

    Print time for data collection

--- DataCollectionA.py
+++ DataCollectionA.py
@@ -1,6 +1,6 @@
 # script for getting the data from the api, removing unneccessary keys and converts it into a (for the program) readable dict
 
-import pandas
+import pandas, time
 
 keys_for_analyzing = { # keys which will be kept
     "NY": {
@@ -50,12 +50,13 @@ keys_for_analyzing = { # keys which will be kept
 } 
 
 data_bases = { # Structure: "City(short)": api # website
-    "NY": "https://data.cityofnewyork.us/resource/qgea-i56i.json?$limit=1000", # https://data.cityofnewyork.us/Public-Safety/NYPD-Complaint-Data-Historic/qgea-i56i/about_data
+    "NY": "https://data.cityofnewyork.us/resource/qgea-i56i.json?$limit=1", # https://data.cityofnewyork.us/Public-Safety/NYPD-Complaint-Data-Historic/qgea-i56i/about_data
     "LA": "https://data.lacity.org/resource/63jg-8b9z.json?$limit=1000", #https://data.lacity.org/Public-Safety/Crime-Data-from-2010-to-2019/63jg-8b9z/about_data
 }
 
 # get the Dataframe of choosen city with only valid coloums (to analyze)
 def getDataFrame(city: str):
+    start_time = time.time()
     data_base = None # data base from city crime api - none because value will be tried to set with 'try'
     label_conversion = None # conversion table (keys_for_analyzing) - also being set with 'try'
 
@@ -80,4 +81,5 @@ def getDataFrame(city: str):
     data_base.drop(to_drop, axis=1, inplace=True) # removing not eligible keys
     data_base = data_base.rename(columns=label_conversion) # rename keys to (for program) readable format
     
+    print(f"Total time for DataCollection (secs): {time.time() - start_time}")
     return data_base
